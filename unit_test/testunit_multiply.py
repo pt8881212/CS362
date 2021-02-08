e51fc30afc6a299c3ba0643017fa85f358ca373e
Pratiksha <50192288+pt8881212@users.noreply.github.com>
2021-02-07 19:06:31 -0800

    Update testunit_multiply.py

--- unit_test/testunit_multiply.py
+++ unit_test/testunit_multiply.py
@@ -5,7 +5,7 @@ class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(calculator.multiply(3,2), 6)
 
-    def test1(self):
+    def test2(self):
         self.assertEqual(calculator.multiply(4,1), 4)
 
 if __name__== '__main__':
